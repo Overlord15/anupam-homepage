9cb32618aecde301078692f08a05565b92a7ce63
Overlord15 <khamrai.eren.3@gmail.com>
2023-02-05 21:59:59 +0530

    404 Page Added

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,15 +1,23 @@
 import Nextlink from 'next/link'
-import { Link } from '@chakra-ui/react'
+import { Container, Text, Link, IconButton, Center, Image } from '@chakra-ui/react'
 import React from 'react'
+import dynamic from 'next/dynamic'
+import Navbar from '../components/Navbar'
+import { ImHome3 } from 'react-icons/im'
 
 const Notfound = () => {
     return (
         <>
-        <Nextlink href="/">
-            <Link href="/">Requested Page Not Found. Return to homepage.</Link>
-        </Nextlink>
+            <Navbar />
+            <Container maxH="80" pt="100" justifyContent={"center"} alignItems={"center"}>
+                <Text textAlign={"center"} >Requested Page Not Found. Click on the below home button to return.</Text>
+                <Center><Image borderRadius="15" py="20" src="/404.gif" /></Center>
+                <Nextlink href="/">
+                    <Center><IconButton px="10" py="5" variant={"outline"} colorScheme={"teal"} aria-label='Home Link' icon={<ImHome3 />} /></Center>
+                </Nextlink>
+            </Container>
         </>
     )
 }
 
-export default Notfound
+export default dynamic(() => Promise.resolve(Notfound), { ssr: false })
